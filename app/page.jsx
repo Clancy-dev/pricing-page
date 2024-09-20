087e1e038f5675a7729e646e78f6bd947c836aee
Clancy-dev <132342072+Clancy-dev@users.noreply.github.com>
2024-09-20 11:44:21 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import PricingCard from '@/components/PricingCard'
+import React from 'react'
+
+export default function page() {
+  return (
+    <section className='section-one'>
+      <PricingCard/>
+      <PricingCard/>
+      <PricingCard/>
+               
+
+    </section>
+  )
+}
---
+++ components/PricingCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function PricingCard() {
+  return (
+    <div>
+        <div className="pricing-card">
+
+
+            h
+        </div>
+      
+    </div>
+  )
+}
